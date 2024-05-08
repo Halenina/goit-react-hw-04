77d62dc708f92765c3f21d109056cab92172147d
Halenina <galenina20032101@gmail.com>
2024-05-09 01:50:42 +0300

    Update ImageModal.jsx

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,44 +1,33 @@
 import css from "./ImageModal.module.css";
-import Modal from "react-modal";
+import ReactModal from "react-modal";
 
-Modal.setAppElement("#root");
+ReactModal.setAppElement("#root");
 
-const customStyles = {
-  overlay: {
-    backgroundColor: "rgba(0, 0, 0, 0.8)",
-  },
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    transform: "translate(-50%, -50%)",
-    background: "none",
-    backgroundSize: "cover",
-    overflow: "none",
-    border: "none",
-    maxWidth: "calc(100vw - 20px)",
-    maxHeight: "calc(100vh - 20px)",
-    padding: "0px",
-  },
+const ImageModal = ({
+  onImgModalOpen,
+  onImgModalClose,
+  srcImgModal,
+  description,
+  likes,
+  author,
+}) => {
+  return (
+    <ReactModal
+      className={css.Modal}
+      overlayClassName={css.Overlay}
+      isOpen={onImgModalOpen}
+      onRequestClose={onImgModalClose}
+    >
+      <img className={css.imgModal} src={srcImgModal} alt="" />
+      <div className={css.imgDescription}>
+        <p>{description}</p>
+        <div className={css.imgInfo}>
+          <p>Likes: {likes}</p>
+          <p>Author: {author}</p>
+        </div>
+      </div>
+    </ReactModal>
+  );
 };
 
-export default function ImageModal({ openModal, selectedImage, onClose }) {
-  return (
-    <Modal
-      isOpen={openModal}
-      style={customStyles}
-      shouldCloseOnOverlayClick={true}
-      shouldCloseOnEsc={true}
-      onRequestClose={onClose}
-    >
-      {selectedImage && (
-        <img
-          className={css.image}
-          src={selectedImage}
-          alt={selectedImage.alt_description}
-        />
-      )}
-    </Modal>
-  );
-}
+export default ImageModal;
